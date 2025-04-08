45a2c1f9eacfdd64f3829672c7e9013ad7b919f1
nataliaN <asdfghmax@yandex.ru>
2025-04-09 00:07:12 +0300

    sum-of-positive

--- 8kyu/sum-of-positive.js
+++ 8kyu/sum-of-positive.js
@@ -9,15 +9,24 @@
 // assert.strictEqual(positiveSum([-1,-2,-3,-4,-5]),0);
 // assert.strictEqual(positiveSum([-1,2,3,4,-5]),9);
 // ✅SOLUTION
-function positiveSum(arr) {
-    let sum = 0;
-    for(let i = 0; i < arr.length; i++) {
-        if(arr[i] > 0) {
-            sum += arr[i];
-        }
-    }
-    return sum;
-}
+// function positiveSum(arr) {
+//     let sum = 0;
+//     for(let i = 0; i < arr.length; i++) {
+//         if(arr[i] > 0) {
+//             sum += arr[i];
+//         }
+//     }
+//     return sum;
+// }
 
-console.log(positiveSum([-1,2,3,4,-5]));
+// function positiveSum(arr) {
+//     return arr.reduce((a,b)=> a + (b > 0 ? b : 0),0);
+// }
+
+// function positiveSum (arr) {
+//     return arr.filter(x => x>=0).reduce((a, c) => a + c, 0);
+// }
+
+const positiveSum = (arr) => arr.reduce((sum, n) => n > 0 ? sum + n : sum, 0);
+console.log(positiveSum([-1,-2,-3,-4,-5]));
 
