5a4fef9f71c4be4d4c10d423b8718d046f314157
nataliaN <asdfghmax@yandex.ru>
2025-04-13 17:28:36 +0300

    getAverage-of-marks

---
+++ 8kyu/get-the-mean-of-an-array.js
@@ -0,0 +1,16 @@
+// Description:
+// It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+// Return the average of the given array rounded down to its nearest integer.
+// The array will never be empty.
+// Test.assertEquals(getAverage([2,2,2,2]),2);
+// Test.assertEquals(getAverage([1,2,3,4,5,]),3);
+// Test.assertEquals(getAverage([1,1,1,1,1,1,1,2]),1);
+function getAverage(marks){
+    let sum = 0;
+    for (let i = 0; i < marks.length; i++) {
+        sum += marks[i];
+    }
+    return Math.floor(sum / marks.length);
+}
+
+console.log(getAverage([6,8,7,5,6,6,5,10,8,11,18,19,15]))
