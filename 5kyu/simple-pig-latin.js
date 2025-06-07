3d5a374a2261cc9595e3a5bb4cb8f882a8a26db0
nataliaN <asdfghmax@yandex.ru>
2025-06-07 17:49:09 +0300

    pigIt

--- 5kyu/simple-pig-latin.js
+++ 5kyu/simple-pig-latin.js
@@ -4,9 +4,27 @@
 // pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
 // pigIt('Hello world !');     // elloHay orldway !
 
-function pigIt(str){
-    return str.split(' ').map(item => item.slice(1) + item[0] + 'ay').join(' ')
+function pigIt(str) {
+    return str.split(' ').map(item => {
+        let count = 0;
+        for (let i = 0; i < item.length; i++) {
+            if (/[a-zA-Z]/.test(item[i])) {
+                count++
+            }
+        }
+        if (count === item.length) {
+            return (item.slice(1) + item[0] + 'ay')
+        } else {
+            return item;
+        }
+    }).join(' ')
 }
 
 console.log(pigIt('Pig latin is cool'))
 console.log(pigIt('Hello world !'))
+
+// function pigIt(str) {
+//     return str.replace(/\w+/g, (w) => {
+//         return w.slice(1) + w[0] + 'ay';
+//     });
+// }
